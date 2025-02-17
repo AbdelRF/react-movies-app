f7e6443d0729e3a4ce60648d2854bdc5e2228e7b
Abderrahim <abderrahim.bouaassoul@gmail.com>
2025-02-17 22:30:32 +0100

    Changed BrowserRouter to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import './css/App.css'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from "./Pages/Home"
-import Favorites from './Pages/Favorites'
-import NavaBar from './components/NavaBar'
-import { MovieProvider } from './contexts/MovieContext'
+import './css/App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./Pages/Home";
+import Favorites from './Pages/Favorites';
+import NavaBar from './components/NavaBar';
+import { MovieProvider } from './contexts/MovieContext';
 
 function App() {
   return (
     <MovieProvider>
       <NavaBar />
-    <main className='main-content'>
-    <Router>
-    <Switch>
-        <Route path='/' element={<Home />} />
-        <Route path='/favorites' element={<Favorites />} />
-    </Switch>
-    </Router>
-    </main>
+      <main className='main-content'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Routes>
+        </Router>
+      </main>
     </MovieProvider>
-  )
+  );
 }
 
-export default App
+export default App;
