c9c0a059c02352f2881dd4a0059d0a9eb3306842
Abderrahim <abderrahim.bouaassoul@gmail.com>
2025-02-17 22:38:20 +0100

    Fixing Routing issue for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './css/App.css';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./Pages/Home";
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 import Favorites from './Pages/Favorites';
 import NavaBar from './components/NavaBar';
 import { MovieProvider } from './contexts/MovieContext';
@@ -9,13 +9,11 @@ function App() {
   return (
     <MovieProvider>
       <NavaBar />
-      <main className='main-content'>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/favorites' element={<Favorites />} />
-          </Routes>
-        </Router>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
       </main>
     </MovieProvider>
   );
